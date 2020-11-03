9e3d85767733cf83399f20f63109b3948b7bd7d8
Ian <qq860831@gmail.com>
2020-11-03 09:26:51 +0800

    ok

--- all.js
+++ all.js
@@ -3,4 +3,18 @@ $(document).ready(function (){
     $('.hamburger').click(function(e){
         $('.navbar').toggleClass('show');
     });
+
+    // sidebar折疊式選單
+    $('.list').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('list-active')
+        $(this).parent().siblings().find('a').removeClass('list-active')
+    });
+
+    // 愛心選擇
+    $('.fa-heart-o').click(function(e){
+        e.preventDefault(); //去掉a連結的作用
+        $(this).toggleClass(' fa fa-heart-o');
+        $(this).toggleClass(' fa fa-heart');
+    });
 });
